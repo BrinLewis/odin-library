d445eaf3f628d8a8fa3125be0242707ec5471fad
BrinLewis <brinsley22@hotmail.co.uk>
2022-12-20 14:52:41 +0000

    Create a new function to render the cards Add if statement to check if book has been read when rendering

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 let myLibrary = [];
 
-myLibrary[0] = testBook;
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -9,36 +7,49 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
 
-function displayBooks() {
-  for (const book of myLibrary) {
-    const card = document.createElement("div");
-    card.classList.add("bookCard");
-    const container = document.querySelector(".books");
-    container.appendChild(card);
+function renderCard(book) {
+  const card = document.createElement("div");
+  card.classList.add("bookCard");
+  const container = document.querySelector(".books");
+  container.appendChild(card);
 
-    const title = document.createElement('p');
-    const author = document.createElement('p');
-    const pages = document.createElement('p');
-    const read = document.createElement('p');
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const read = document.createElement("p");
 
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = book.pages;
-    read.textContent = book.read;
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
 
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(read);
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(read);
 
+  if (book.read === true) {
+    read.textContent = "Read";
+    read.setAttribute("style", "background-color: green");
+  } else {
+    read.textContent = "Un-read";
+    read.setAttribute("style", "background-color: red");
   }
 }
 
+function displayBooks() {
+  for (const book of myLibrary) {
+    renderCard(book); //Loop through the array and create a card for each book
+  }
+}
+
+// Manually added books for testing purposes
+addBookToLibrary("Harry Potter", "JK Rowling", 456, false);
+addBookToLibrary("LOTR", "Tolkien", 2265, true);
 displayBooks(myLibrary);
 
 // Open a form with 4 inputs, name, author, pages, read
