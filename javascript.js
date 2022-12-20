f08f82ed8a06a3fe396d014f11133d2365798a88
BrinLewis <brinsley22@hotmail.co.uk>
2022-12-20 10:36:20 +0000

    Add book object constructor Create function to add book object to library array

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
+let myLibrary = [];
 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+// Open a form with 4 inputs, name, author, pages, read
+// When the submit button is clicked, take these inputs and store them in the array as a new book
